90bd82b100ccb2c22e09453088eb2c530e8f14f4
Peter Effeney <effeneys@gmail.com>
2019-04-06 16:36:37 +1000

    Initial commit

---
+++ lib/src/todo_list/todo_list_component.dart
@@ -0,0 +1,43 @@
+import 'dart:async';
+
+import 'package:angular/angular.dart';
+import 'package:angular_components/angular_components.dart';
+
+import 'todo_list_service.dart';
+import 'material_dropdown_select_simple_component.dart';
+
+@Component(
+  selector: 'todo-list',
+  styleUrls: ['todo_list_component.css'],
+  templateUrl: 'todo_list_component.html',
+  directives: [
+    MaterialCheckboxComponent,
+    MaterialFabComponent,
+    MaterialIconComponent,
+    materialInputDirectives,
+    NgFor,
+    NgIf,
+    MaterialDropdownSelectSimpleDemoComponent,
+  ],
+  providers: [ClassProvider(TodoListService)],
+)
+class TodoListComponent implements OnInit {
+  final TodoListService todoListService;
+
+  List<String> items = [];
+  String newTodo = '';
+
+  TodoListComponent(this.todoListService);
+
+  @override
+  Future<Null> ngOnInit() async {
+    items = await todoListService.getTodoList();
+  }
+
+  void add() {
+    items.add(newTodo);
+    newTodo = '';
+  }
+
+  String remove(int index) => items.removeAt(index);
+}
